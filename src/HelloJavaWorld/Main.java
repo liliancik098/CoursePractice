7d2dd4afe9dc9af9893c752b7c399286beb36867
lilian <lilianzau@mail.ru>
2022-01-31 16:56:52 +0200

    first program

---
+++ src/HelloJavaWorld/Main.java
@@ -0,0 +1,10 @@
+package HelloJavaWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("Lilian");
+        System.out.println(user.getName());
+    }
+}
---
+++ src/HelloJavaWorld/User.java
@@ -0,0 +1,14 @@
+package HelloJavaWorld;
+
+public class User {
+
+    String name;
+
+    public String getName() {
+        return "Hello " + name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
