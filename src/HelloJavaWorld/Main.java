4a2968ef382c3398e3fd19ce150a5f127f5efe6f
lilian <lilianzau@mail.ru>
2022-02-02 18:30:51 +0200

    homeword 1

--- src/HelloJavaWorld/Main.java
+++ src/HelloJavaWorld/Main.java
@@ -1,10 +1,18 @@
 package HelloJavaWorld;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        User user = new User();
-        user.setName("Lilian");
-        System.out.println(user.getName());
+//        User user = new User();
+//        user.setName("Lilian");
+//        System.out.println(user.getName());
+
+        Scanner time = new Scanner(System.in);
+        System.out.println("introduce");
+        long min = time.nextLong();
+        System.out.println(min);
+
     }
 }
---
+++ src/Variables/Main.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class Main {
+    public static void main(String[] args) {
+        long currentWorldPopulation = 7811080815L;
+        int carsProducesThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        float Pi = 3.14159F;
+        System.out.println("Current world population = " + currentWorldPopulation);
+        System.out.println("Cars produces this year = " + carsProducesThisYear);
+        System.out.println("First letter in alphabet is: " + firstLetterInAlphabet);
+        System.out.println("Number PI = " + Pi);
+        System.out.println("Java is amazing? " + javaIsAmazing);
+    }
+}
