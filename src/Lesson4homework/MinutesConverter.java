54aa32c117d52f019ef47aca87431f0a8271d20c
lilian <lilianzau@mail.ru>
2022-02-02 19:16:20 +0200

    homework minutes converter

--- src/Lesson4homework/MinutesConverter.java
+++ src/Lesson4homework/MinutesConverter.java
@@ -1,7 +1,19 @@
 package Lesson4homework;
 
+import java.util.Scanner;
+
 public class MinutesConverter {
     public static void main(String[] args) {
-        
+        long minutes;
+        double days;
+        double years;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please input minutes: ");
+        minutes = input.nextLong();
+        days = minutes / 1.440;
+        years = minutes * 1.9013E-6;
+        System.out.println(minutes + " minutes in days is: " + days);
+        System.out.println(minutes + " minutes in years is: " + years);
+
     }
 }
