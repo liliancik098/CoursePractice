09a1af3d0d5df15fc15cdd0bea2c7d9343b909f2
Lilian <lilian@royalsystems.dev>
2022-02-09 11:35:38 +0200

    exercise 3 lesson 7

---
+++ src/com/homework/lesson7/Inversion.java
@@ -0,0 +1,28 @@
+package com.homework.lesson7;
+
+/**
+ * Write a program that prompts user for a positive integer. The program
+ * shall read the input as int; and print the "reverse" of the input integer.
+ * @author Pletosu Lilian
+ */
+
+import java.util.Scanner;
+
+public class Inversion {
+    public static void main(String[] args) {
+        numInversion();
+    }
+
+    public static void numInversion() {
+        int number, reverse = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the integer number: ");
+        number = input.nextInt();
+        while (number != 0) {
+            int rest = number % 10;
+            reverse = reverse * 10 + rest;
+            number = number / 10;
+        }
+        System.out.println("Your reverse number is: " + reverse);
+    }
+}
