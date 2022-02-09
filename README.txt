1acac38c161a9863c08ced35d365309bce321983
Lilian <lilian@royalsystems.dev>
2022-02-09 09:58:00 +0200

    exercise 1 - lesson 7

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
 My fisrt README
-
+img
 testing
---
+++ src/com/homework/lesson7/DivisibleNum.java
@@ -0,0 +1,28 @@
+package com.homework.lesson7;
+
+/***
+ * Create a program that displays all numbers from 100 to 1,000, ten per
+ * line, which are divisible by 5 and 6. The numbers are separated by exactly one
+ * space
+ * @author Pletosu Lilian
+ */
+
+public class DivisibleNum {
+    public static void main(String[] args) {
+        divisibleBy5And6();
+    }
+
+    public static void divisibleBy5And6() {
+        int count = 0;
+        for (int nr = 100; nr <= 1000; nr++) {
+            if (nr % 5 == 0 && nr % 6 == 0) {
+                System.out.print(nr + " ");
+                count++;
+            }
+            if (count == 10) {
+                System.out.println();
+                count = 0;
+            }
+        }
+    }
+}
