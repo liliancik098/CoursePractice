74c4827e120f4883211a1d817566dbc5b02aa16c
Lilian <lilian@royalsystems.dev>
2022-02-11 10:10:04 +0200

    exercise 3 lesson 8, create a cloneArray method

---
+++ src/com/homework/lesson8/CopyArray.java
@@ -0,0 +1,20 @@
+package com.homework.lesson8;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] array1 = {34, 56, 78, 90};
+        int[] array2 = new int[array1.length];
+        cloneArray(array1, array2);
+    }
+
+
+    static void cloneArray(int[] array1, int[] array2) {
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = i; j < array2.length; j++) {
+                array2[j] = array1[i];
+                System.out.print(array2[j] + " ");
+                break;
+            }
+        }
+    }
+}
